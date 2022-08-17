4998df582a1a0b60918c8ef44522632116da70f2
Valentinas <valentinas.olekas@gmail.com>
2022-08-17 14:22:26 +0300

    Update

---
+++ resources/tutorial/server/Client/Resursas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AltV.Net.Client;
+
+namespace ExampleProject
+{
+    internal class ExampleResource : Resource
+    {
+        public override void OnStart()
+        {
+            Console.WriteLine("Client Started");
+        }
+
+        public override void OnStop()
+        {
+            Console.WriteLine("Client Stopped");
+        }
+    }
+}
